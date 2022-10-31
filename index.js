ebc7bf4477cbeb709c75edb8eade1dc108865ee6
Andrés Esteban Rodríguez Jiménez <andrudeluxe@gmail.com>
2022-10-31 17:32:20 -0500

    predeploy

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
-const port = process.env.PORT || 3000;;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whilelist = ['http://localhost:8080', 'https://mi-app.com'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
 	origin: (origin, callback) => {
-		if(whilelist.includes(origin) || !origin) {
+		if(whitelist.includes(origin) || !origin) {
 			callback(null, true)
 		} else {
 			callback(new Error('no permitido'))
